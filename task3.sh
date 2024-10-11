292476ff3eb869c5bc14970f2746d603b9969ef5
Nayan Taori <64462883+NDT2000@users.noreply.github.com>
2024-10-10 22:12:00 -0400

    Update task3.sh

--- task3.sh
+++ task3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-gawk -F',' 'NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") {print $0} } ' titanic.csv | \
-sed 's/ male/ ,M/g; s/ female/ ,F/g' | \
-gawk -F',' '$7 != "" {total += $7; count++ } END { print total/count; }'
+gawk -F',' 'NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") {print $0} }' titanic.csv | \
+sed 's/male/M/g; s/female/F/g' | \
+gawk '{print $0}' 
+
+gawk -F',' 'NR > 1 { if ($3 == 2 && substr($13, 1, 1) == "S") { if($7 != "") {total += $7; count++ }} } END { if (count > 0) print "Average Age: ", total/count; }' titanic.csv
+
